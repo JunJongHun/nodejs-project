5a1f05eb0f496cc0345bd0acecededab9547b21a
penloo <44522204+penloo@users.noreply.github.com>
2022-04-20 00:23:14 +0900

    4-20

--- src/controllers/uploadController.js
+++ src/controllers/uploadController.js
@@ -1,3 +1,3 @@
 export const upload = (req, res, next) => {
   return res.send("show graph");
-};
+};                                                  //이것도 콜백함수 되것네   
--- src/routers/uploadFileRouter.js
+++ src/routers/uploadFileRouter.js
@@ -1,8 +1,8 @@
-import express from "express";
-import { upload } from "../controllers/uploadController.js";
+import express from "express";             //express모듈로부터 가져옴 
+import { upload } from "../controllers/uploadController.js";       // uploadcontroller 에 만든 객체 upload를 여기로부터 가져온다 
 
-const uploadFileRouter = express.Router();
+const uploadFileRouter = express.Router();        //express 라우터 호출 
 
-uploadFileRouter.get("/", upload);
+uploadFileRouter.get("/", upload);       //주어진 URL에 대해 GET 요청에서  /를 실행
 
-export default uploadFileRouter;
+export default uploadFileRouter;  //exprot default는  한모듈덩 허너
